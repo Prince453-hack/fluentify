39552ac0d65c8751cfb9e6717b18ab36c6733256
RandomGod <63667312+Prince453-hack@users.noreply.github.com>
2024-04-30 00:26:20 +0530

    ....

--- app/(main)/courses/page.tsx
+++ app/(main)/courses/page.tsx
@@ -6,6 +6,7 @@ const CoursesPage = async () => {
   return (
     <div className="h-full max-w-[912px] px-3 mx-auto">
       <h1 className="text-2xl font-bold text-neutral-700">Language Courses</h1>
+      <h1 className="text-xl mt-4 font-bold">Select a course to continue</h1>
       <List courses={courses} activeCourseId={1} />
     </div>
   );
