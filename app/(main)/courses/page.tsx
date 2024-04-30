d6366fddecea214ac2a26fa4457a5b2d2db74a07
RandomGod <63667312+Prince453-hack@users.noreply.github.com>
2024-04-30 20:32:33 +0530

    some ui changes

--- app/(main)/courses/page.tsx
+++ app/(main)/courses/page.tsx
@@ -1,13 +1,15 @@
-import { getCourses } from "@/db/queries";
+import { getCourses, getUserProgress } from "@/db/queries";
 import List from "./list";
 
 const CoursesPage = async () => {
   const courses = await getCourses();
+  const userProgress = await getUserProgress();
+
   return (
     <div className="h-full max-w-[912px] px-3 mx-auto">
       <h1 className="text-2xl font-bold text-neutral-700">Language Courses</h1>
       <h1 className="text-xl mt-4 font-bold">Select a course to continue</h1>
-      <List courses={courses} activeCourseId={1} />
+      <List courses={courses} activeCourseId={userProgress?.activeCourseId} />
     </div>
   );
 };
--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -3,8 +3,16 @@ import StickyWrapper from "@/components/sticky-wrapper";
 import React from "react";
 import Header from "./header";
 import UserProgress from "@/components/user-progress";
+import { getUserProgress } from "@/db/queries";
+import { redirect } from "next/navigation";
 
-const Learn = () => {
+const Learn = async () => {
+  const userProgress = await getUserProgress();
+
+  if (!userProgress || !userProgress.activeCourseId) {
+    redirect("/courses");
+  }
+
   return (
     <div className="flex flex-row-reverse gap-[48px] px-6">
       <StickyWrapper>
